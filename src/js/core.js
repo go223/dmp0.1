6a0be5887332d4a5907d8d76dce1298d5e2312d3
go223 <huangbin_223@qq.com>
2017-10-19 20:10:58 +0800

    0.002 black

--- src/js/core.js
+++ src/js/core.js
@@ -1,18 +1,54 @@
+/* 调用动画方法
+$('选择器').animateCss('动画名')
+*/
+$.fn.extend({
+    animateCss: function(animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationedn animationend';
+        this.addClass('animated ' + animationName).one(animationEnd,function(){
+            $(this).removeClass('animated ' + animationName);
+        });
+        return this;
+    }
+})
+/* 调用时间 $('选择器').showNowTime() */
+$.fn.extend({
+    showNowTime:function(className){
+        var newDate = new Date(),
+            year = newDate.getFullYear(),
+            month = newDate.getMonth() + 1,
+            day = newDate.getDate(),
+            hours = newDate.getHours(),
+            minutes = newDate.getMinutes(),
+            second = newDate.getSeconds();
+            month = month < 10 ? '0'+month : month;
+            day = day < 10 ? '0'+day : day;
+            hours = hours < 10 ? '0'+hours : hours;
+            minutes = minutes < 10 ? '0'+minutes : minutes;
+            second = second < 10 ? '0'+second : second;
+        var result = year+'-'+month+'-'+day+' '+hours+':'+minutes+':'+second;
+        this.addClass('yb-show-now-time').text(result);
+    }
+});
 $(function(){
     //mock数据
     Mock.mock(
         'http://mockjs',{
             
-            'number|1000-2000': 1,
-            'number1|1000-2000': 1,
-            'number2|1000-2000': 1,
-            'number3|1000-2000': 1,
-            'total|1000-2000':1
+            'ywxt|200-900': 1,
+            'sjb|2999-9000': 1,
+            'bbiao|1999-4000': 1,
+            'zbgs|1080-2000': 1,
+            'total|1000-2000':1,
+            'array|5-15':[
+                {
+                    'msg|+1':["爬虫","eHR","OA","IOT","log","现场管理","文旅","租赁","预算","电表","新媒体","销售额","温控","儿童娱乐","商服","客流","物业","商管"]
+                }
+            ]
         }
     );
     //数据流动清洗
     setInterval(function(){
-        $('.dataClear-canvas').barrager(
+        /* $('.dataClear-canvas').barrager(
             [
                 {"msg":"爬虫"},
                 {"msg":"eHR"},
@@ -33,7 +69,18 @@ $(function(){
                 {"msg":"物业"},
                 {"msg":"商管"}
             ]
-        );
+        ); */
+        $.ajax({
+            url:'http://mockjs',
+            dataType:'json',
+            type:'get'
+        }).done(function(req){
+            $('.dataClear-canvas').barrager(req.array);
+            $('.ywxt-num').text(req.ywxt);
+            $('.sjb-num').text(req.sjb);
+            $('.bbiao-num').text(req.bbiao);
+            $('.zbgs-num').text(req.zbgs);
+        })
     },2000); 
     //横向数字流动
     //$('.dataStatistics').dataStatistics({})
@@ -41,4 +88,9 @@ $(function(){
     
     
     $('.dataStatistics').ybCount({url:"http://mockjs"});
+    clearInterval(nowtimer);
+    var nowtimer = setInterval (function () {
+        $('.nowtime').showNowTime();
+    },1000);
+    
 });
